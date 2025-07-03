31a8fe298e9f0290dbb3c1d1200963c6bd7be988
Lin Thit Htoo <lth1212001@gmail.com>
2025-07-03 23:16:22 +0630

    -

--- custom-service-discovery.Api/Models/BlogModel.cs
+++ custom-service-discovery.Api/Models/BlogModel.cs
@@ -1,11 +1,10 @@
-﻿namespace custom_service_discovery.Api.Models
+﻿namespace custom_service_discovery.Api.Models;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-        public bool IsDeleted { get; set; }
-    }
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool IsDeleted { get; set; }
 }
