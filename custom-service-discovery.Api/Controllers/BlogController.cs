cec9e150bd60a8a6371a2a8ab1e7b3a34b0b3399
Lin Thit Htoo <lth1212001@gmail.com>
2025-07-03 23:16:00 +0630

    -

--- custom-service-discovery.Api/Controllers/BlogController.cs
+++ custom-service-discovery.Api/Controllers/BlogController.cs
@@ -1,28 +1,27 @@
 ﻿using custom_service_discovery.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace custom_service_discovery.Api.Controllers
-{
-    [Route("api/Blog")]
-    [ApiController]
-    public class BlogController : ControllerBase
-    {
-        [HttpGet("GetBlogs")]
-        public IActionResult GetBlogs()
-        {
-            var lst = new List<BlogModel>()
-            {
-                new BlogModel()
-                {
-                    BlogId = 1,
-                    BlogTitle = "Test",
-                    BlogAuthor  = "Test",
-                    BlogContent= "Test",
-                    IsDeleted = false
-                }
-            };
+namespace custom_service_discovery.Api.Controllers;
 
-            return Ok(lst);
-        }
+[Route("api/Blog")]
+[ApiController]
+public class BlogController : ControllerBase
+{
+    [HttpGet("GetBlogs")]
+    public IActionResult GetBlogs()
+    {
+        var lst = new List<BlogModel>()
+        {
+            new BlogModel()
+            {
+                BlogId = 1,
+                BlogTitle = "Test",
+                BlogAuthor  = "Test",
+                BlogContent= "Test",
+                IsDeleted = false
+            }
+        };
+
+        return Ok(lst);
     }
 }
